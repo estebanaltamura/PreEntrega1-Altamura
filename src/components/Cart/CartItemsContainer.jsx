c34c6689c24e31b7edf7d667b0d4c38eba5fb242
yo <esteban.altamura@gmail.com>
2023-03-20 15:45:30 -0300

    actualizacion

--- src/components/Cart/CartItemsContainer.jsx
+++ src/components/Cart/CartItemsContainer.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { Link } from "react-router-dom"
 import { CartContext } from "../Contexts/CartContextProvider"
+import { BsBank } from "react-icons/bs";
 import "./CartItemsContainer.css"
 
 import { ItemListCart } from "./ItemListCart"
@@ -19,13 +20,24 @@ export const CartItemsContainer = ()=>{
                             <div className="contenedorItems redondeado">
                                 <ItemListCart />
                             </div>
-            
-                            <Link className="botonesCarro botonesCarroConItems pagarConMPButton" to="/Payment">
-                                <img src="https://i.postimg.cc/Xv5j8NDx/icono_mercadopago.png" className="mercadopagoicono" alt="" />
-                                CONTINUE TO CHECKOUT
-                            </Link>
-            
-                            <Link className="botonesCarro botonesCarroConItems" to="/home">CONTINUE SHOPPING</Link> 
+
+                            <div className="paymentButtonsContainer">                                
+                                <h4>Credit card payments</h4>
+                                <Link className="botonesCarro MPButton" to="/Payment">
+                                    <img src="https://i.postimg.cc/Xv5j8NDx/icono_mercadopago.png" className="mercadopagoicono" alt="" />
+                                    CONTINUE TO CHECKOUT
+                                </Link>
+
+                                <h4>Bank transfer payment method</h4>
+                                <Link className="botonesCarro customOrderButton" to="/home">
+                                    
+                                    CREATE CUSTOM ORDER
+                                </Link>
+
+                                <div className="line"></div>
+                
+                                <Link className="botonesCarro" to="/home">CONTINUE SHOPPING</Link>                                 
+                            </div>
                         </main>
                     </>
 
@@ -34,8 +46,8 @@ export const CartItemsContainer = ()=>{
                     <>
                         <main className="mainContainerCart">
                             <div className="emptyCartMessageAndButtonContainer">
-                                <h2 className="emptyCartMessage">Your cart is currently empty</h2>
-                                <Link className="botonesCarro botonesCarroSinItems" to="/home">CONTINUE SHOPPING</Link>                    
+                                <h2>Your cart is currently empty</h2>
+                                <Link className="botonesCarro" to="/home">CONTINUE SHOPPING</Link>                    
                             </div>
                         </main>
                     </>
