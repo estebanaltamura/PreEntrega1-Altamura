c35a7de72a9222bea7be40658aeb3193b1d3fc69
yo <esteban.altamura@gmail.com>
2023-03-22 11:31:33 -0300

    actualizacion

--- src/components/OrderCreated/OrderCreated.js
+++ src/components/OrderCreated/OrderCreated.js
@@ -1,13 +1,32 @@
-import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
 import "./OrderCreated.css"
 
 export const OrderCreated = ()=>{
 
     const {orderId} = useParams()
+    const [seconds, setSeconds] = useState(5)
+    const history = useNavigate()
+
+    useEffect(()=>{
+        const interval = setInterval(()=>setSeconds(seconds-1), 1000)
+        
+        if (seconds == 0){
+            clearInterval(interval)
+            history("/home")
+        }
+    },[seconds])
+
     return(
         <div className="orderCreatedContainer">
 
-            <h3>{`Order number ${orderId} created. In 24-48 hours we are going to contact you for payment details and delivery`}</h3>
+            <h3>{`Order number ${orderId} created`}</h3>
+            <br/> 
+            <h3>{`In 24-48 hours we are going to contact you for payment details and delivery`}</h3>
+            <br/> 
+            <h3>{`Redirenting to home page in ${seconds}`}</h3>
+            
+            
         </div>
     )
 }
