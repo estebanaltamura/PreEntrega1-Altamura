d25952549b00433163d6d3e9d056f17086290082
yo <esteban.altamura@gmail.com>
2023-03-23 09:51:46 -0300

    actualizacion

--- src/components/CustomOrder/Form.js
+++ src/components/CustomOrder/Form.js
@@ -89,20 +89,20 @@ export const Form = ()=>{
                                 
                                     <div className="inputContainer">
                                         <span className="subtitle">NAME:</span>
-                                        <input type="text" name="fullName" onKeyUp={resetAlerts} />
+                                        <input autocomplete="off" type="text" name="fullName" onKeyUp={resetAlerts} />
                                         <span className="inputAlerts">{fullNameAlert}</span>
                                     </div>
 
                                     <div className="inputContainer">
                                         <span className="subtitle">PHONE:</span>  
-                                        <input type="text" name="phone" onKeyUp={resetAlerts} />
+                                        <input autocomplete="off" type="text" name="phone" onKeyUp={resetAlerts} />
                                         {phoneAlert && <TbHelp className="helpIcon" onClick={onClickHandlerHelpIcon}/>}
                                         <span className="inputAlerts">{phoneAlert}</span>
                                     </div>
                                     
                                     <div className="inputContainer">
                                         <span className="subtitle">MAIL:</span>  
-                                        <input type="text" name="mail" onKeyUp={resetAlerts} />
+                                        <input autocomplete="off" type="text" name="mail" onKeyUp={resetAlerts} />
                                         <span className="inputAlerts">{mailAlert}</span>
                                     </div>
                                     
