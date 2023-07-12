f985a9b559670c377f0352286466654e470dc1ce
colaboradorAltamura <xoxotrends1@gmail.com>
2023-07-12 20:23:57 -0300

    actualizacion

--- src/components/Categories/Item.js
+++ src/components/Categories/Item.js
@@ -8,10 +8,10 @@ export const Item = ({image, name, price, id, index})=>{
     const {idCollection} = useParams()   
 
     
-
+    
     return(
         <Link className={`card card${index+1}`} to={`/products/${idCollection}/${id}`}>
-            <img className={`imagenCard${index + 1} imagenCard`} src={image} alt="foto"></img>
+            <img className={`imagenCard${index + 1} imagenCard`} src={image} alt="foto" onLoad={()=>console.log(index + 1, "cargo")}></img>
             <p className={`tituloCard${index + 1} tituloCard`}>{name}</p>
             <p className={`precioCard${index + 1} precioCard`}>{`$${price}`}</p> 
         </Link>            
