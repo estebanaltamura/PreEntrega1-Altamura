58952c4e304816111e870cf31f651df56a9bef4d
Andres <esteban.altamura@gmail.com>
2024-07-12 20:19:38 +0200

    update

--- src/services/external/useMercadoPagoService.js
+++ src/services/external/useMercadoPagoService.js
@@ -1,33 +1,30 @@
-export const useMercadoPagoService = ()=>{  
-  const getUrl = async (items)=>{
-    const endPoint = "https://www.encrypted-chat-backend.online:5000/createOrder"  
-    
-    try{
+export const useMercadoPagoService = () => {
+  const getUrl = async (items) => {
+    const endPoint = 'https://www.internal-server-projects5000/createOrder';
+
+    try {
       const data = await fetch(endPoint, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          
         },
-        mode: 'cors',        
-        
+        mode: 'cors',
+
         body: JSON.stringify({
-          items
-        })
-      })
-  
-      if(data.status === 200){
-        const json = await data.json()
-        return json
-      }
-      else return false;   
+          items,
+        }),
+      });
+
+      if (data.status === 200) {
+        const json = await data.json();
+        return json;
+      } else return false;
+    } catch (error) {
+      return false;
     }
-    catch(error){
-      return false      
-    }
-  }  
-    
-  return({
-    getUrl    
-  })
-}
+  };
+
+  return {
+    getUrl,
+  };
+};
